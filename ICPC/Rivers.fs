450122052fe8e7f61d1e2b4f0eef72b68a526d56
Bonani Brian Wogqoyi <bbwogqoyi@outlook.com>
2020-05-23 02:07:25 +0200

    Refactoring and arranging functions. Small clean-up of the codebase aswell

--- ICPC/Rivers.fs
+++ ICPC/Rivers.fs
@@ -3,12 +3,12 @@ module Utils =
   open System
 
   type Info = {
-    line_width:int
+    width:int
     length:int
   }
 
-  type Head = {
-    coord: (int*int)
+  type River = {
+    head: (int*int)
     path: (int*int) list
   }
 
@@ -35,27 +35,20 @@ module Utils =
     | _ -> a
 
   /// Given a char separator and string, it splits it up into List of parts
-  let stringSplit (input:string) (separator:char) (splitOption: SplitOption) = 
+  let stringSplit (input:string) (separator:char) (option: SplitOption) = 
     let words = input.Split separator |> List.ofArray
-    match splitOption with
+    match option with
     | Default -> words
     | RemoveEmpty -> words |> List.filter (fun x-> String.length x > 0)
 
   /// Given a sentance/paragraph as a single string, it finds length of the longest word
   let findLongestWordLength (words:string list) =
-    let largestNumber (iLen:int) (cLen:int) = 
-      match iLen < cLen with
-      | true -> cLen
-      | _ -> iLen
-
-    List.fold (fun maxLength word -> largestNumber maxLength (String.length word) ) 0 words
+    let helper maxLength word = max maxLength (String.length word)  
+    List.fold helper 0 words
 
   /// Given a list of strings, checks if an empty entries exists 
   let emptyEntriesExist (words:string list) : bool =
-    let result = List.tryFind (fun word -> String.Empty=word) words 
-    match result with
-    | None -> false
-    | Some _ -> true
+    List.exists (fun word -> String.Empty=word) words
 
   /// Given two strings, concatenates and returns the new string
   let stringConcat (state:string) (entry:string) =
@@ -90,37 +83,26 @@ module Utils =
     helper 0 []
 
   /// Imagine a list of strings as jaggered matrix, this finds all the whitespaces and rep
-  let flattenListToCoordinates (words:string list) =
+  let flattenToCoordinates (words:string list) =
     let rec helper words index _out =
       match words with 
       | [] -> _out
       | h::t -> 
         let coords = (getWhitespaceCoordinates h index)@_out
         helper t (index+1) coords
-    helper words 0 []
+    List.sort ( helper words 0 [] )
 
-  // Searchs a list with (x,y) coordinate entries, return an adjacent entry to the input
-  let tryFindAdjacentFromList coords (r, c) = 
-    let rec helper candidates entry =
-      match candidates, entry=None with
-      | [], _ | _, false -> entry
-      | (row, column)::t, _ ->  
-        let entry = List.tryFind (fun coordEntry -> coordEntry = (row, column) ) coords
-        helper t entry
-
-    let row = r+1
-    let candidates = (row, c-1)::(row, c)::(row, c+1)::[]
-    helper candidates None
-
-  let merge (a:Head) (b:Head) =
-    let (r0,_), (r1,_) = a.coord, b.coord
+  /// Takes 2 River's, joins them and gets the new head
+  let merge (a:River) (b:River) =
+    let (r0,_), (r1,_) = a.head, b.head
     let _out = List.sort (List.distinct (a.path@b.path))
     match r0>r1 with
     | true -> { a with path=_out }
     | false -> { b with path=_out }
 
+  /// Adds new river to the collection, ensuring all entries are unique
   let appendDistinct key _out = 
-    let predicate func entry = func entry.coord key.coord
+    let predicate func entry = func entry.head key.head
     match (List.tryFind (predicate (=))  _out) with
     | None -> key::_out
     | Some v -> 
@@ -129,23 +111,20 @@ module Utils =
       let rest = (List.filter (predicate (<>)) _out)
       entry::rest
 
-  // Checks if (x,y) coordinate entries are adjacent to each other
+  /// Checks if (x,y) coordinate entries are adjacent to each other
   let coordsAjacent (r0, c0) (r1, c1) : bool = 
     let isUnder = abs(r1-r0) = 1 
     let isAdjacent = abs(c1-c0) = 0 || abs(c1-c0) = 1
     (isUnder && isAdjacent)
 
-  (*
-     |roses_are_red|
-     |eish*I*like__|*
-     *So, two is the maximum number of adacent items
-  *)
-  let newFlowEntry (entries:(int*int) list) = // --- FIXME ----
-    let key = List.last entries
-    { coord=key; path=entries }
+  /// Creates a new instance of 'River' record
+  let newFlowEntry (entries:(int*int) list) =
+    let key = List.last (List.sort entries)
+    { head=key; path=entries }
 
-  let rec updateFlowEntries (key:int*int) (entries:(int*int) list) (_out:Head list) =
-    let predicate key entry = coordsAjacent key.coord entry.coord
+  /// Updates rivers; joining and growth
+  let rec updateFlowEntries (key:int*int) (entries:(int*int) list) (_out:River list) =
+    let predicate key entry = coordsAjacent key.head entry.head
     match _out with
     | [] -> (newFlowEntry (key::entries))::[]
     | _ ->
@@ -156,8 +135,9 @@ module Utils =
       | Some v -> 
         let newEntry = merge entry v
         let _newOut = appendDistinct newEntry _out
-        List.filter (fun item -> item.coord <> v.coord) _newOut
+        List.filter (fun item -> item.head <> v.head) _newOut
 
+  // Using a set of (x,y) coordinates, finds all the possible paths
   let pathfinder coords = 
     let rec helper _in _out =
       match _in with
@@ -172,51 +152,37 @@ module Utils =
             let _left = updateFlowEntries key [a] _out
             let _right = updateFlowEntries key [b] _out
             List.distinct (_left@_right)
-
         helper rest _newOut
-
     let _results = helper coords []
     let maxLen = List.fold (fun len x -> max (List.length x.path) len) 0 _results
     let _reduced = List.filter (fun x -> (List.length x.path)>=maxLen) _results
     _reduced
-    
-  let getLengthOfLongest (words:string list) =
-    let folder count entry = 
-      max (List.length entry.path) count
 
-    let rec helper coords maxLength =
-      match coords with
-      | [] -> maxLength
-      | _ ->
-        let _out = pathfinder coords        
-        List.fold folder maxLength _out
-
-    let coords = List.rev (flattenListToCoordinates words)
-    helper coords 0 
+  /// Finds the longest river path and returns its length
+  let getLongestRiverPath (rivers:River list) =
+    let maxPath (count:int) (entry:River) =
+      max count (List.length entry.path)
+    List.fold maxPath 0 rivers
 
   let searchForRivers (lineWidth:int) (words:string list) (maxWidth:int)=
     let rec helper width _info _min=
-      let resized = reshape width words
-      let length = getLengthOfLongest resized
+      let wordGrid = reshape width words
+      let coordinates = flattenToCoordinates wordGrid
+      let riverPaths = pathfinder coordinates
+      let length = getLongestRiverPath riverPaths
       let _out =  
         match length>_min with 
-        | true -> { line_width=width; length=length}::_info
+        | true -> { width=width; length=length}::_info
         | false -> _info
-      match (List.length resized)<_min || width>=maxWidth with
+
+      match (List.length wordGrid)<_min || width>=maxWidth with
       | true -> _out
       | _ ->helper (width+1) _out (max _min length)
-    let _results = helper lineWidth [] 0
-    let _default = { line_width=lineWidth; length=0 }
-    let _max = List.fold (fun a b -> maxInfo a b) _default _results
-    _max.line_width, _max.length
 
-  //let searchForRivers (lineWidth:int) (words:string list) _ =
-  //  let resized = reshape 23 words
-  //  let length = getLengthOfLongest resized
-  //  length
-  ////  ////let _default = { line_width=lineWidth; length=0 }
-  ////  ////let _max = List.fold (fun a b -> maxInfo a b) _default _results
-  ////  ////_max.line_width, _max.length
+    let _results = helper lineWidth [] 0
+    let _default = { width=lineWidth; length=0 }
+    let _max = List.fold (fun a b -> maxInfo a b) _default _results
+    _max.width, _max.length
 
   let processInput (input:string) =
     let words = stringSplit input ' ' Default
@@ -234,6 +200,3 @@ module Utils =
     | true -> 
       let maxWidth = (String.length input)
       Some (searchForRivers longestWordLength words maxWidth)
-
-//processInput "The Yangtze is the third longest river in Asia and the longest in the world to flow entirely in one country"
-//processInput "When two or more rivers meet at a confluence other than the sea the resulting merged river takes the name of one of those rivers"
